92b8b65dadcef48266ca37098e74e8a190a9768f
theo <teddyhoover@gmail.com>
2020-12-25 13:03:04 -0600

    commit tree be3e7fb995edf5a1a680372031799631347b284a

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,6 +1,10 @@
 require 'pry'
 
 class Artist
+  extend Memorable
+  extend Findable
+  include Paramable
+
   attr_accessor :name
   attr_reader :songs
 
@@ -11,20 +15,8 @@ class Artist
     @songs = []
   end
 
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
-  end
-
   def self.all
     @@artists
-  end
-
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
   end
 
   def add_song(song)
